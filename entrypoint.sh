8e3cb430ff6964a68225e2c87d670de32dfd0ed1
Igor Rodionov <goruha@gmail.com>
2022-10-22 00:11:23 +0300

    Secret output (#2)
    ## what
    * Secret output
    
    ## why
    * https://nitratine.net/blog/post/how-to-pass-secrets-between-runners-in-github-actions/

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+set -e
+
+case "${OPERATION}" in
+	encode)
+		result=$(gpg --symmetric --batch --passphrase "${SECRET}" --output - <(echo "${IN}") | base64 -w0)
+		echo "out=${result}" >> $GITHUB_OUTPUT
+		;;
+
+	decode)
+		result=$(gpg --decrypt --quiet --batch --passphrase "${SECRET}" --output - <(echo "${IN}" | base64 -d))
+		echo "::add-mask::${result}"
+		echo "out=${result}" >> $GITHUB_OUTPUT
+		;;
+
+	*)
+		echo $"op input can be only {encode|decode}"
+		exit 1
+esac
